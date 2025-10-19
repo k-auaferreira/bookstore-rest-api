97201c587af98e91299cca54d445473037e74c81
k-auaferreira <kaua.fs@outlook.com>
2025-10-19 13:12:01 -0300

    init: first commit

---
+++ src/main/java/br/com/fiap/bookstorerestapi/BookstoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookstorerestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookstoreRestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/bookstorerestapi/BookstoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.bookstorerestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
